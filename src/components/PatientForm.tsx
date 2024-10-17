5e9db4eaf84a9edb512a347af49ae30d56a6ca32
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-10-17 11:59:33 -0400

    step 7

--- src/components/PatientForm.tsx
+++ src/components/PatientForm.tsx
@@ -1,6 +1,82 @@
-
 export default function PatientForm() {
+  
   return (
-    <div>PatietForm</div>
+    <div className="md:w-1/2 lg:w-2/5 mx-5">
+        <h2 className="font-black text-3xl text-center">Patient Monitoring</h2>
+
+        <p className="text-lg mt-5 text-center mb-10">
+            Add Patients and {''}
+            <span className="text-indigo-600 font-bold">Manage them</span>
+        </p>
+
+        <form 
+            className="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
+            noValidate
+        >
+              <div className="mb-5">
+                  <label htmlFor="name" className="text-sm uppercase font-bold">
+                      Patient
+                  </label>
+                  <input  
+                      id="name"
+                      className="w-full p-3  border border-gray-100"  
+                      type="text" 
+                      placeholder="Patient Name" 
+                  />
+              </div>
+
+              <div className="mb-5">
+                <label htmlFor="caretaker" className="text-sm uppercase font-bold">
+                    Owner 
+                </label>
+                <input  
+                    id="caretaker"
+                    className="w-full p-3  border border-gray-100"  
+                    type="text" 
+                    placeholder="Owner Name" 
+                />
+              </div>
+
+            <div className="mb-5">
+              <label htmlFor="email" className="text-sm uppercase font-bold">
+                  Email 
+              </label>
+              <input  
+                  id="email"
+                  className="w-full p-3  border border-gray-100"  
+                  type="email" 
+                  placeholder="Email" 
+              />
+            </div>
+
+            <div className="mb-5">
+                <label htmlFor="date" className="text-sm uppercase font-bold">
+                    Discharged Date
+                </label>
+                <input  
+                    id="date"
+                    className="w-full p-3  border border-gray-100"  
+                    type="date" 
+                />
+            </div>
+            
+            <div className="mb-5">
+                <label htmlFor="symptoms" className="text-sm uppercase font-bold">
+                Symtoms 
+                </label>
+                <textarea  
+                    id="symptoms"
+                    className="w-full p-3  border border-gray-100"  
+                    placeholder="Patient Symtoms" 
+                ></textarea>
+            </div>
+
+            <input
+                type="submit"
+                className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors"
+                value='Save Patient'
+            />
+        </form> 
+    </div>
   )
 }
