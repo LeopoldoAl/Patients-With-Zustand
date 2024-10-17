d83ec4f811942b7632fc289961745a7912efb85d
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-10-17 11:30:57 -0400

    step 4

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,12 @@ function App() {
 
   return (
     <>
-      <h1 className="text-4xl">Pacientes</h1>
+      <div className="container mx-auto mt-20">
+        <h1 className="font-black text-5xl text-center md:w-2/3 md:mx-auto">
+          Patient Monitoring {''}
+          <span className="text-indigo-700">Vet</span>
+        </h1>
+      </div>
     </>
   )
 }
