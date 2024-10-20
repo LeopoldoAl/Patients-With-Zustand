f8084c4cfe266395d58ec2e945a3251a9da3651b
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-10-20 11:57:16 -0400

    step 27

--- src/components/PatientsList.tsx
+++ src/components/PatientsList.tsx
@@ -1,5 +1,9 @@
+import { usePatientStore } from "../store/store"
 
 export default function PatientsList() {
+  const patients = usePatientStore(state => state.patients)
+
+  console.log(patients)
   return (
     <div>PatientsList</div>
   )
