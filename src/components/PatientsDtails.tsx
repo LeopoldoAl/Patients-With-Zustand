b6aa16a5194a1d769fb9cc2c272e4440c5bb429b
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-10-21 09:08:12 -0400

    step 32

--- src/components/PatientsDtails.tsx
+++ src/components/PatientsDtails.tsx
@@ -14,6 +14,21 @@ export default function PatientsDtails({patient}: PatientsDtailsProps) {
         <PatientDetailItem label="Email" data={patient.email} />
         <PatientDetailItem label="Discharged Date" data={patient.date.toString()} />
         <PatientDetailItem label="Symptoms" data={patient.symptoms} />
+
+        <div className=" flex justify-between mt-10">
+          <button
+            type='button'
+            className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg"
+          >
+            Edit
+          </button>
+          <button
+            type='button'
+            className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg"
+          >
+            Delete
+          </button>
+        </div>
     </div>
   )
 }
