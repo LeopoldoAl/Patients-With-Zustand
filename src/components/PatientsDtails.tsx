3c0f91e5a06dd7ee5db8245ec61b60fb5aef95d1
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-10-21 08:51:17 -0400

    step 31

--- src/components/PatientsDtails.tsx
+++ src/components/PatientsDtails.tsx
@@ -1,3 +1,4 @@
+import PatientDetailItem from "./PatientDetailItem"
 import { Patient } from "./types"
 
 type PatientsDtailsProps = {
@@ -6,8 +7,13 @@ type PatientsDtailsProps = {
 
 export default function PatientsDtails({patient}: PatientsDtailsProps) {
   return (
-    <div>
-        {patient.name}
+    <div className="mx-5 my-10 px-5 py-10 bg-white shadow-md rounded-xl">
+        <PatientDetailItem label="ID" data={patient.id} />
+        <PatientDetailItem label="Name" data={patient.name} />
+        <PatientDetailItem label="Owner" data={patient.caretaker} />
+        <PatientDetailItem label="Email" data={patient.email} />
+        <PatientDetailItem label="Discharged Date" data={patient.date.toString()} />
+        <PatientDetailItem label="Symptoms" data={patient.symptoms} />
     </div>
   )
 }
