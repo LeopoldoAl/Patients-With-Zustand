732fe85e663c8e7e40dd52482e54170ca055665c
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-10-21 10:48:56 -0400

    step 38

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,6 @@
-import {v4 as uuidv4} from 'uuid'
+import {v4 as uuidv4} from 'uuid';
 import { create } from "zustand";
+import { devtools } from 'zustand/middleware';
 import { DraftPatient, Patient } from "../components/types";
 
 
@@ -15,23 +16,25 @@ const createPatient = (patient: DraftPatient): Patient => {
     return {...patient, id: uuidv4()}
 }
 
-export const usePatientStore = create<PatientState>((set) => ({
-    patients: [],
-    activeId: '',
-    addPatient: (data) => {
-        const newPatient = createPatient(data)
-       set((state) => ({
-        patients: [...state.patients, newPatient]
-       }))
-    },
-    deletePatient: (id) => {
-        set((state) => ({
-            patients: state.patients.filter( patient => patient.id !== id)
-        }))
-    },
-    getPatientById: (id) => {
-        set(() => ({
-            activeId: id
-        }))
-    }
-}))
+export const usePatientStore = create<PatientState>()(
+    devtools((set) => ({
+            patients: [],
+            activeId: '',
+            addPatient: (data) => {
+                const newPatient = createPatient(data)
+            set((state) => ({
+                patients: [...state.patients, newPatient]
+            }))
+            },
+            deletePatient: (id) => {
+                set((state) => ({
+                    patients: state.patients.filter( patient => patient.id !== id)
+                }))
+            },
+            getPatientById: (id) => {
+                set(() => ({
+                    activeId: id
+                }))
+            }
+    })
+))
