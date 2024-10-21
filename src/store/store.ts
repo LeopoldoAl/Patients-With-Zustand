f0a35c85322ff06721f6cafa7b69b87767ecbe0b
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-10-21 10:13:10 -0400

    step 36

--- src/store/store.ts
+++ src/store/store.ts
@@ -5,8 +5,10 @@ import { DraftPatient, Patient } from "../components/types";
 
 type PatientState = {
     patients: Patient[],
+    activeId: Patient['id'],
     addPatient: (data: DraftPatient) => void,
-    deletePatient: (id: Patient['id']) => void
+    deletePatient: (id: Patient['id']) => void,
+    getPatientById: (id: Patient['id']) => void
 }
 
 const createPatient = (patient: DraftPatient): Patient => {
@@ -15,6 +17,7 @@ const createPatient = (patient: DraftPatient): Patient => {
 
 export const usePatientStore = create<PatientState>((set) => ({
     patients: [],
+    activeId: '',
     addPatient: (data) => {
         const newPatient = createPatient(data)
        set((state) => ({
@@ -24,6 +27,11 @@ export const usePatientStore = create<PatientState>((set) => ({
     deletePatient: (id) => {
         set((state) => ({
             patients: state.patients.filter( patient => patient.id !== id)
+        }))
+    },
+    getPatientById: (id) => {
+        set(() => ({
+            activeId: id
         }))
     }
 }))
